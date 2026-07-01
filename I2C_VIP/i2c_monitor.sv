5d5049a36819a651a8e45b42c987d62129c376c5
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-07-01 10:26:17 +0530

    Update i2c_monitor.sv

--- I2C_VIP/i2c_monitor.sv
+++ I2C_VIP/i2c_monitor.sv
@@ -1,5 +1,5 @@
 // collects the data passively and give the data to the scoreboard and subscriber through analysis port
-`define MON_VIF mon_vif.driver
+`define MON_VIF mon_vif.monitor
 class i2c_monitor extends uvm_monitor;
 
   `uvm_component_utils(i2c_monitor)
@@ -13,21 +13,29 @@ class i2c_monitor extends uvm_monitor;
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!(`uvm_config_db#(virtual i2c_inf)::get(this,"","vif",mon_vif)))
-      `uvm_error("MONITOR","interface not get",UVM_HIGH)
+    if(!(`uvm_config_db#(virtual i2c_inf)::get(this,"","vif",mon_vif)))begin
+      `uvm_fatal("MONITOR","interface not get")
+    end
       mon_ap=new("mon_ap",this); //analysis port object creation
-    seq=seq_item::type_id::create("seq");
+    
   endfunction
 
+  task mon_run();
+    
+    seq=seq_item::type_id::create("seq");
+    bit [6:0] temp_addr; //to collect the address
+    bit [7:0] iaddr_q; //to collect internal address
+    bit [7:0] data_q; //data capturing
+    bit nack=0; //to track early exit
   //start detection 
   forever begin
-    @(negedge sda);
+    @(negedge `MON_VIF.sda);
     if(`MON_VIF.scl==1)
         break;
   end
   
   //collecting 7bit address
-  reg [6:0] temp_addr;
+  
     for(int i=6;i>=0;i--)begin
       @(posedge `MON_VIF.scl);
       temp_addr[i]=`MON_VIF.sda;
@@ -38,21 +46,65 @@ class i2c_monitor extends uvm_monitor;
   @(posedge `MON_VIF.scl);
   seq.rd_wr=`MON_VIF.sda;
   
-  //1 bit ACK
+  //1 bit ADDR_ACK
+  @(posedge `MON_VIF.scl);
+    seq.addr_ack = (`MON_VIF.sda==0);
+    if(!seq.addr_ack) begin
+      nack = 1;
+      `uvm_info("MON","ADDR NACK received - skipping to STOP",UVM_LOW);
+    end
+  
   //collecting 8bit internal address 
-  //1 bit ACK
+  
+    if(!nack) begin
+  for(int i=7;i>=0;i--)begin
+   @(posedge `MON_VIF.scl);
+    iaddr_q[i]=`MON_VIF.sda;
+  end
+  seq.int_addr=iaddr_q;
+  
+  //1 bit internal address ACK
+  @(posedge `MON_VIF.scl);
+  seq.int_ack = (`MON_VIF.sda==0);
+      if(!seq.int_ack) begin
+      nack =1;
+      `uvm_info("MON","INT_ADDR NACK received go to STOP",UVM_LOW)
+    end
+    end
+    
+  //capturing data
+  
+    if(!nack) begin
+  for(int i=7;i>=0;i--) begin
+    @(posedge `MON_VIF.scl);
+    data_q[i] = `MON_VIF.sda;
+  end
   //WRITE : 8 data bits [master] + 1 ACK [slave]
+  if(seq.rd_wr==0)
+    seq.wdata =data_q;
   //read :8 data bits [slave] + 1 ACK [master]
+  else
+    seq.rdata =data_q;
+    //data ack
+  @(posedge `MON_VIF.scl);
+    seq.data_ack = (`MON_VIF.sda == 0);
+    end
   //stop detection
     forever begin
-      @(posedge sda);
-      if(scl==1)
+      @(posedge `MON_VIF.sda);
+      if(`MON_VIF.scl==1)
         break;
-  end
+    end
+  
   // package everything into packet/transaction object, write to analysis port.
+  mon_ap.write(seq);
+  endtask
   
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
+    forever begin
+    mon_run();
+    end
   endtask
 
 endclass
