6e4c75f4d63724d61a3b94ced26873e90a92f9f3
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-07-01 11:59:58 +0530

    Update i2c_tb_top.sv

--- I2C_VIP/i2c_tb_top.sv
+++ I2C_VIP/i2c_tb_top.sv
@@ -2,31 +2,31 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-`include "Slave_7b.sv"
+`include "design.sv"
 //`include "Slave_10b.sv"
-`include "interface.sv"
+`include "i2c_interface.sv"
 `include "tb_pkg.sv"
 
 module top;
   
-  intf vif;
+  intf vif();
   Slave_7b#(4) sl1(vif.sda, vif.scl);
   //Slave_10b#(8) sl2(intf.sda, intf.scl);
-  assign vif.sda=1;
-  assign vif.scl=1;
+/*  assign vif.sda=1;
+  assign vif.scl=1;*/
   initial begin
-    uvm_config_db#(virtual intf)::set(uvm_root::get(),"*","driver",intf);
-    uvm_config_db#(virtual intf)::set(uvm_root::get(),"*","mon_vif",intf);
+    uvm_config_db#(virtual intf)::set(uvm_root::get(),"*","driver",vif);
+    uvm_config_db#(virtual intf)::set(uvm_root::get(),"*","mon_vif",vif);
   end
   
-  initial run_test("test");
+  initial run_test("i2c_test");
   
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars;
 //     #500 $finish;
   end
-  
+  /*
   // To check clock streching
   initial begin
     #62 @(negedge vif.scl);
@@ -36,5 +36,5 @@ module top;
     force vif.scl=0;
     #16 release vif.scl;
   end
-  
+  */
 endmodule
